1e03a25cdd1281e1e1f2cf55c2dba5cf4999fcc8
Mike Evans <mikee@saxicola.co.uk>
2018-11-14 10:38:10 +0000

    Tweaking layout.

--- src/label_print.py
+++ src/label_print.py
@@ -5,24 +5,24 @@ Print PAT labels on supplied sheet.
 '''
 
 import labels
+import os
 from reportlab.graphics import shapes
 import reportlab.lib.pagesizes
 from reportlab.platypus import  Table, TableStyle
 
-
 # Create an custom portrait  sheet with 3 columns and 7 rows of
 # labels. Each label is 90mm x 25mm with a 2mm rounded corner. The margins are
 # automatically calculated.
-specs = labels.Specification(131, 196, 3, 7, 38, 25, corner_radius=2)
+specs = labels.Specification(131, 198, 3, 7, 38, 25, corner_radius=2)
 
 
 def draw_label(label, width, height, obj):
     # Just convert the object to a string and print this at the bottom left of
     # the label.
-    label.add(shapes.String(5, 25,  str(obj[0]), fontName="Helvetica", fontSize=8))
+    label.add(shapes.String(8, 25,  str(obj[0]), fontName="Helvetica", fontSize=8))
     label.add(shapes.String(80, 25, str(obj[1]), fontName="Helvetica", fontSize=8))
-    label.add(shapes.String(5, 2,   str(obj[2]), fontName="Helvetica", fontSize=8))
-    label.add(shapes.String(55, 2,  str(obj[3]), fontName="Helvetica", fontSize=8))
+    label.add(shapes.String(8, 4,   str(obj[2]), fontName="Helvetica", fontSize=8))
+    label.add(shapes.String(55, 4,  str(obj[3]), fontName="Helvetica", fontSize=8))
 
 # Create the sheet.
 sheet = labels.Sheet(specs, draw_label, border=True)
@@ -44,3 +44,7 @@ sheet.add_label(obj)
 # Save the file and we are done.
 sheet.save('basic.pdf')
 print("{0:d} label(s) output on {1:d} page(s).".format(sheet.label_count, sheet.page_count))
+# This SHOULD scale and print, but it doesn't
+#lpr -o fit-to-page -o InputSlot=ManualAdj -o media=Custom.130x195mm basic.pdf
+# Or after modding the ppd file.
+#lpr -o InputSlot=ManualAdj  -o media=Statement basic.pdf
