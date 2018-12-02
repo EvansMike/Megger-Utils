5bb8d0d1dff4a2f947fcb6383deb7b0dd7a3fa48
Mike Evans <mikee@saxicola.co.uk>
2018-12-02 19:31:25 +0000

    Added a setup.py

---
+++ megger/__main__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+ """bmegger.__main__: executed when bootstrap directory is called as script."""
+from .megger_cap import main
+main()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='megger',
+      version='0.1',
+      description='Megger utility',
+      #url='http://github.com/storborg/funniest',
+      author='Mike Evans',
+      author_email='mikee@saxicola.co.uk',
+      license='MIT',
+      packages=['megger'],
+      entry_points = {'console_scripts': ['megger_cap=megger.megger_cap:main',],},
+      zip_safe=False
+      )
