abd1bc7d66f7fab4a153b987c136ac3815394995
Mike Evans <mikee@saxicola.co.uk>
2018-12-02 20:12:00 +0000

    Indent.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(name='megger',
-      version='0.1',
-      description='Megger utility',
-      #url='http://github.com/storborg/funniest',
-      author='Mike Evans',
-      author_email='mikee@saxicola.co.uk',
-      license='MIT',
-      packages=['megger'],
-      entry_points = {'console_scripts': ['megger_cap=megger.megger_cap:main',\
-            'mk_merge=megger.mk_merge:main'],},
-      zip_safe=False
-      )
+    version='0.1',
+    description='Megger PAT testing utility',
+    #url='http://github.com/storborg/funniest',
+    author='Mike Evans',
+    author_email='mikee@saxicola.co.uk',
+    license='MIT',
+    packages=['megger'],
+    entry_points = {
+        'console_scripts': ['megger_cap=megger.megger_cap:main',\
+        'mk_merge=megger.mk_merge:main'],
+        },
+    zip_safe=False
+    )
